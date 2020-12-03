9691d532d0dc772073a4c2f51c55e2348ec8f6db
Valentin <44469298+Infinity-Kim@users.noreply.github.com>
2020-12-03 14:45:58 +0300

    Update Font Size

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -5,14 +5,14 @@ module.exports = () => {
             .pipe($.glp.sourcemaps.init())
             .pipe($.glp.sass().on('error', $.glp.sass.logError))
             .pipe($.glp.shorthand())
+            .pipe($.glp.groupCssMediaQueries())
             .pipe($.glp.postcss([
                 $.autoprefixer({
                     cascade: false,
                     overrideBrowserslist: ['> 5%', 'last 2 versions', 'ie > 7']
                 }),
-                $.cssnano()
+                $.cssnano({})
             ]))
-            .pipe($.glp.groupCssMediaQueries())
             .pipe($.glp.sourcemaps.write())
             .pipe($.gulp.dest('./docs/assets/css'))
             .pipe($.browserSync.stream());
