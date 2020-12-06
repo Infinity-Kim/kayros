a3b52495e90e3d568629e31c32f122268741ddd4
Valentin <44469298+Infinity-Kim@users.noreply.github.com>
2020-12-07 01:14:40 +0300

    test

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,8 +1,17 @@
 'use strict';
 
 const width = document.documentElement.clientWidth;
+//
+// if (+width >= 1216) {
+//     const swiper2 = new Swiper('.swiper-2', {
+//         slidesPerView: 4,
+//         spaceBetween: 25,
+//         mousewheel: true
+//     });
+// }
+
 if (+width >= 992) {
-    const swiper = new Swiper('.swiper-container', {
+    const swiper = new Swiper('.swiper-1', {
         pagination: {
             el: '.swiper-pagination',
             type: 'progressbar',
@@ -12,11 +21,21 @@ if (+width >= 992) {
         spaceBetween: 20,
         mousewheel: true
     });
+    // const swiper2 = new Swiper('.swiper-2', {
+    //     slidesPerView: 4,
+    //     spaceBetween: 25,
+    //     mousewheel: true
+    // });
 } else {
-    const swiper = new Swiper('.swiper-container', {
+    const swiper = new Swiper('.swiper-1', {
         loop: true,
         slidesPerView: 1.5,
         spaceBetween: 20,
+        mousewheel: true
+    });
+    const swiper2 = new Swiper('.swiper-2', {
+        slidesPerView: 1.5,
+        spaceBetween: 25,
         mousewheel: true
     });
 }
