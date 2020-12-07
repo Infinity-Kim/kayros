70d89131f2737ecd64dc4cfeda63bc6ebcda2028
Valentin <44469298+Infinity-Kim@users.noreply.github.com>
2020-12-07 19:37:28 +0300

    add item

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -4,7 +4,6 @@ const width = document.documentElement.clientWidth;
 const swiper2 = new Swiper('.swiper-2', {
     slidesPerView: 'auto',
     spaceBetween: 25,
-    mousewheel: true
 });
 
 if (+width >= 992) {
@@ -17,11 +16,6 @@ if (+width >= 992) {
         spaceBetween: 20,
         mousewheel: true
     });
-    // const swiper2 = new Swiper('.swiper-2', {
-    //     slidesPerView: 4,
-    //     spaceBetween: 25,
-    //     mousewheel: true
-    // });
 } else {
     const swiper = new Swiper('.swiper-1', {
         slidesPerView: 1.5,
