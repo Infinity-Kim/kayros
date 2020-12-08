0c8fa0d8b4439ea88a8f17a13bf2b84c6289c54b
Valentin <44469298+Infinity-Kim@users.noreply.github.com>
2020-12-08 17:33:02 +0300

    add copyright

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const width = document.documentElement.clientWidth;
+const btnTop = document.querySelector('.top');
+let requestId;
 const swiper2 = new Swiper('.swiper-2', {
     slidesPerView: 'auto',
     spaceBetween: 25,
@@ -23,4 +25,16 @@ if (+width >= 992) {
         mousewheel: true
     });
 }
+
+const up = () => {
+    let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+    if (top > 0) {
+        window.scrollBy(0,-100);
+        requestId = requestAnimationFrame(up);
+    } else {
+        cancelAnimationFrame(requestId);
+    }
+}
+
+btnTop.addEventListener('click', up);
 
